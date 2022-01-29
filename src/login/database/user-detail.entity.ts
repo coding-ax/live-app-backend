03cb6a88ef13d792dfb255e07ebabca86d3ba7e9
ax <codingax@foxmail.com>
2022-01-29 21:42:23 +0800

    feat: add cookie decaration

---
+++ src/login/database/user-detail.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class UserDetail {
+    @PrimaryColumn()
+    open_id: string;
+
+    @Column()
+    nick_name: string;
+
+    @Column()
+    signature: string;
+
+    @Column()
+    avatar_url: string;
+}
