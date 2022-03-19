0b479c62c3ecaacf4db62b88e6435c19340f389b
ax <codingax@foxmail.com>
2022-03-19 16:50:32 +0800

    feat: add file upload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { initEnv } from './env';
 
 async function bootstrap() {
+  initEnv();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser('live-app'));
   await app.listen(3000);
