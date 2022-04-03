ec1de81282eb0c15cde8361a809b496646913580
ax <codingax@foxmail.com>
2022-04-03 16:53:17 +0800

    feat: add node_env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { initEnv } from './env';
 async function bootstrap() {
   initEnv();
   const app = await NestFactory.create(AppModule);
+  process.env.NODE_ENV === 'development' && app.enableCors();
   app.use(cookieParser('live-app'));
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
