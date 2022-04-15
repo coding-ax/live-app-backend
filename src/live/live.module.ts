742247345ad6a1b683ac9a8357cc3b01ae0e8422
ax <codingax@foxmail.com>
2022-04-15 17:20:33 +0800

    feat: add update time

--- src/live/live.module.ts
+++ src/live/live.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginModule } from 'src/login/login.module';
+import { BarrageDetail } from './database/barrage-detail.entity';
+import { Barrage } from './database/barrage.entity';
 import { LiveDetail } from './database/live-detail.entity';
 import { LiveController } from './live.controller';
 import { LiveService } from './live.service';
 
-const typeOrmFeatureEntities = [LiveDetail];
+const typeOrmFeatureEntities = [LiveDetail, Barrage, BarrageDetail];
 @Module({
   imports: [TypeOrmModule.forFeature(typeOrmFeatureEntities), LoginModule],
   controllers: [LiveController],
